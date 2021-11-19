2ec3bca2501c3b039fcf67b9e060fc314122fd90
rwxmad <rwxmad@gmail.com>
2021-11-19 08:44:50 +0300

    update nvim-autopairs config, other plugins, remove vim-stylus

--- .config/nvim/lua/config/completion.lua
+++ .config/nvim/lua/config/completion.lua
@@ -27,14 +27,10 @@ cmp.setup {
   },
 }
 
-require('nvim-autopairs.completion.cmp').setup({
-  map_cr = true, --  map <CR> on insert mode
-  map_complete = true, -- it will auto insert `(` (map_char) after select function or method item
-  auto_select = true, -- automatically select the first item
-  insert = false, -- use insert confirm behavior instead of replace
-  map_char = { -- modifies the function or method delimiter by filetypes
-    all = '(',
-    tex = '{'
-  }
-})
+-- If you want insert `(` after select function or method item
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
+
+-- add a lisp filetype (wrap my-function), FYI: Hardcoded = { "clojure", "clojurescript", "fennel", "janet" }
+cmp_autopairs.lisp[#cmp_autopairs.lisp+1] = "racket"
 
--- .config/nvim/lua/core/plug.lua
+++ .config/nvim/lua/core/plug.lua
@@ -55,8 +55,6 @@ return require('packer').startup(
 
     use { 'iamcco/markdown-preview.nvim', run = 'cd app && yarn install' }
 
-    use 'iloginow/vim-stylus'
-
     use 'gruvbox-community/gruvbox'
 
     use 'andweeb/presence.nvim'
