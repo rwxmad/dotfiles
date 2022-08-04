4ac86bc42454bd4790796a10a845a4db3dc231ad
rwxmad <rwxmad@gmail.com>
2022-08-04 11:02:16 +0300

    fix(nvim): add completion mappings

--- .config/nvim/lua/config/completion.lua
+++ .config/nvim/lua/config/completion.lua
@@ -11,11 +11,22 @@ cmp.setup {
   },
 
   mapping = {
-    ['<CR>'] = cmp.mapping.confirm({ select = true })
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.close(),
+    ["<C-b>"] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    ["<C-n>"] = cmp.mapping.select_next_item(cmp.SelectBehavior.Insert),
+    ["<C-p>"] = cmp.mapping.select_prev_item(cmp.SelectBehavior.Insert),
   },
   -- lspkind
   formatting = {
       format = lspkind.cmp_format({with_text = true, maxwidth = 50})
+  },
+
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
   },
 
   sources = {
@@ -30,7 +41,4 @@ cmp.setup {
 -- If you want insert `(` after select function or method item
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
-
--- add a lisp filetype (wrap my-function), FYI: Hardcoded = { "clojure", "clojurescript", "fennel", "janet" }
--- cmp_autopairs.lisp[#cmp_autopairs.lisp+1] = "racket"
 
