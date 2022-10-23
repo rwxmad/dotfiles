11450f4f6ca16bbb2025628894b738814fec0d50
rwxmad <rwxmad@gmail.com>
2022-10-23 08:03:12 +0300

    fix(nvim): fix cmp capabilities function

--- .config/nvim/lua/rwxmad/config/lsp/utils.lua
+++ .config/nvim/lua/rwxmad/config/lsp/utils.lua
@@ -3,7 +3,7 @@ local U = {}
 -- LSP servers capabilities w/ nvim-cmp
 function U.capabilities()
   local capabilities = vim.lsp.protocol.make_client_capabilities()
-  return require('cmp_nvim_lsp').update_capabilities(capabilities)
+  return require('cmp_nvim_lsp').default_capabilities(capabilities)
 end
 
 -- Creates LSP mappings
