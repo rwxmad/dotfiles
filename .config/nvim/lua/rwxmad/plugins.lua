084873fccbaf44b2e38bdf2a3c940823e6cc618a
rwxmad <rwxmad@gmail.com>
2022-12-01 09:51:33 +0300

    chore: remove nvim-notify

--- .config/nvim/lua/rwxmad/plugins.lua
+++ .config/nvim/lua/rwxmad/plugins.lua
@@ -159,10 +159,6 @@ return require('packer').startup({
       requires = {
         -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
         'MunifTanjim/nui.nvim',
-        -- OPTIONAL:
-        --   `nvim-notify` is only needed, if you want to use the notification view.
-        --   If not available, we use `mini` as the fallback
-        'rcarriga/nvim-notify',
       },
     })
     use({
