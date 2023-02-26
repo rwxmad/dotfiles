faddbcd8ecde1d649126d24a8226db55a7d1cc91
rwxmad <rwxmad@gmail.com>
2023-02-26 19:49:42 +0300

    chore(nvim): set checkThirdParty to false in lua_ls workspace settings

--- .config/nvim/lua/rwxmad/config/lsp/lspconfig.lua
+++ .config/nvim/lua/rwxmad/config/lsp/lspconfig.lua
@@ -56,6 +56,7 @@ local servers = {
         },
         workspace = {
           library = vim.api.nvim_get_runtime_file('', true),
+          checkThirdParty = false,
         },
         telemetry = {
           enable = false,
