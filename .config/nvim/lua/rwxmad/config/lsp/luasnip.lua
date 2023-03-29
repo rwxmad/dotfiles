b577613c43acf8423aab046fb3bacccc482c4adb
rwxmad <rwxmad@gmail.com>
2023-03-29 09:54:32 +0300

    chore(nvim): add jsdoc to luasnip filetype extend

--- .config/nvim/lua/rwxmad/config/lsp/luasnip.lua
+++ .config/nvim/lua/rwxmad/config/lsp/luasnip.lua
@@ -1,12 +1,12 @@
 require('luasnip').config.setup({
-  history = false
+  history = false,
 })
 
 -- Loading any vscode snippets from plugins
 require('luasnip.loaders.from_vscode').lazy_load()
 
 -- Allow jsx and tsx to use js snippets
-require('luasnip').filetype_extend('javascript', { 'javascriptreact', 'typescriptreact' })
+require('luasnip').filetype_extend('javascript', { 'javascriptreact', 'typescriptreact', 'jsdoc' })
 
 -- Mappins to move around inside snippets
 vim.keymap.set('i', '<C-j>', '<CMD>lua require("luasnip").jump(1)<CR>')
