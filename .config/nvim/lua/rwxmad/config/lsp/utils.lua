e97cb0bfd9ffd44441c209fdc434502529d40084
rwxmad <rwxmad@gmail.com>
2024-04-25 10:04:54 +0300

    chore(nvim): move back go to definition keymap, comment declaration

--- .config/nvim/lua/rwxmad/config/lsp/utils.lua
+++ .config/nvim/lua/rwxmad/config/lsp/utils.lua
@@ -11,8 +11,8 @@ function U.mappings(buf)
   local opts = { noremap = true, silent = true, buffer = buf }
 
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  -- vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  -- vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
   vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
   vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
   -- vim.keymap.set('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
