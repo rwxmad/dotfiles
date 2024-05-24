153276a171d42eb15a1c5d6f553a5e4a00c24292
rwxmad <rwxmad@gmail.com>
2024-05-24 12:10:23 +0300

    fix(nvim): remove filetype option

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -13,8 +13,6 @@ set.clipboard = 'unnamedplus'
 set.encoding = 'utf-8'
 set.fileencoding = 'utf-8'
 
-set.filetype = 'on'
-
 set.showcmd = false
 set.cmdheight = 1
 set.laststatus = 2
@@ -30,6 +28,7 @@ set.backup = false
 set.undofile = true
 
 set.number = true
+set.relativenumber = false
 set.ruler = true
 set.signcolumn = 'yes'
 set.autoindent = true
