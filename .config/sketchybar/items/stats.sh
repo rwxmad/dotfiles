4a969727fcc2916451455165dc1d7e84babfe44c
rwxmad <rwxmad@gmail.com>
2025-06-23 08:35:43 +0300

    feat(sketchybar): move `Stats` config to items dir

---
+++ .config/sketchybar/items/stats.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sketchybar --add alias Stats right \
+           --set Stats background.drawing=on \
+                 background.height="$ITEM_HEIGHT" \
+                 background.color="$ITEM_BACKGROUND" \
+                 background.corner_radius="$ROUNDING"
