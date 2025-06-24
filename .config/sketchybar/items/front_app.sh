c96d4a79ff7efe09798e9b9a9197bec2a7b9363c
rwxmad <rwxmad@gmail.com>
2025-06-24 09:57:08 +0300

    feat(sketchybar): improve `front_app`, add app icon image

--- .config/sketchybar/items/front_app.sh
+++ .config/sketchybar/items/front_app.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 
+source "$CONFIG_DIR/constants.sh"
+
+front_app=(
+  label.align=center
+  label.padding_left=$PADDING
+  label.padding_right=$PADDING
+  label.font="$FONT:Bold:14.0"
+  display=active
+  script="$PLUGIN_DIR/front_app.sh"
+  background.drawing=on
+  background.color="$ITEM_BACKGROUND"
+  background.corner_radius="$ROUNDING"
+  background.height="$ITEM_HEIGHT"
+)
+
 sketchybar --add item chevron left \
            --set chevron icon= label.drawing=off \
            --add item front_app left \
-           --set front_app icon.drawing=off \
-                           script="$PLUGIN_DIR/front_app.sh" \
-                           label.align=center \
-                           background.drawing=on \
-                           background.color="$ITEM_BACKGROUND" \
-                           background.corner_radius="$ROUNDING" \
-                           background.height="$ITEM_HEIGHT" \
+           --set front_app "${front_app[@]}" \
            --subscribe front_app front_app_switched
--- .config/sketchybar/plugins/front_app.sh
+++ .config/sketchybar/plugins/front_app.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+source "$CONFIG_DIR/constants.sh"
+
 # Some events send additional information specific to the event in the $INFO
 # variable. E.g. the front_app_switched event sends the name of the newly
 # focused application in the $INFO variable:
 # https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
 
 if [ "$SENDER" = "front_app_switched" ]; then
-  sketchybar --set "$NAME" label="$INFO"
+	sketchybar --set $NAME label="$INFO" icon.background.drawing=on \
+                                         icon.background.image="app.$INFO" \
+                                         icon.background.image.scale=0.8
 fi
