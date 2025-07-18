f43444946a6cf06a3862dbd23d9d52e66faaf5ac
rwxmad <rwxmad@gmail.com>
2025-07-18 10:59:23 +0300

    feat(sketchybar): remove stats alias item

--- .config/sketchybar/items/stats.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sketchybar --add alias Stats right \
-           --set Stats background.drawing=on \
-                 background.height="$ITEM_HEIGHT" \
-                 background.color="$ITEM_BACKGROUND" \
-                 background.corner_radius="$ROUNDING"
