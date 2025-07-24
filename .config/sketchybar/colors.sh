e8682010aaeba38ba56bfa6383e8bb462de20d2c
rwxmad <rwxmad@gmail.com>
2025-07-24 13:40:05 +0300

    feat(sketchybar): set `everforest` theme

--- .config/sketchybar/colors.sh
+++ .config/sketchybar/colors.sh
@@ -1,9 +1,29 @@
+#!/bin/bash
+
+# palette
+
+# --- everforest ---
+export FG=0xffD3C6AA
+export BG=0xff373F43
+export RED=0xffE67E80
+export ORANGE=0xffE69875
+export YELLOW=0xffDBBC7F
+export GREEN=0xffA7C080
+export AQUA=0xff83C092
+export BLUE=0xff7FBBB3
+export PURPLE=0xffD699B6
+export GREY0=0xff7A8478
+export GREY1=0xff859289
+export GREY2=0xff9DA9A0
+---
+
 export EMPTY_COLOR=0x00000000
 
-export BAR_COLOR=EMPTY_COLOR
+# colors for parts
+export BAR_COLOR=$EMPTY_COLOR
 
-export ITEM_BACKGROUND=0xff51A098
+export ITEM_BACKGROUND=$BG
 
-export ACCENT_COLOR=0xff51A098
+export ACCENT_COLOR=$GREEN
 
-export TEXT_COLOR=0xffffffff
+export TEXT_COLOR=$FG
