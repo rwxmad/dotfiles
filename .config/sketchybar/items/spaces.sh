836e3a67bbd670c6a419ce9319646149a5811f2c
rwxmad <rwxmad@gmail.com>
2025-08-26 15:15:22 +0300

    feat(sketchybar): add symbols to `spaces.sh`

--- .config/sketchybar/items/spaces.sh
+++ .config/sketchybar/items/spaces.sh
@@ -3,7 +3,9 @@
 # https://felixkratz.github.io/SketchyBar/config/components#space----associate-mission-control-spaces-with-an-item
 # to indicate active and available mission control spaces.
 
-SPACE_ICONS=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10")
+# TODO: dynamic icons?
+
+SPACE_ICONS=("1 􀩼" "2 􀎬" "3 􀑬" "4 􁜾" "5 􀒋" "6 􁒃" "7 􀫓" "8 􀢅" "9 􀛸" "10")
 for i in "${!SPACE_ICONS[@]}"
 do
   sid="$(($i+1))"
