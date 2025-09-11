6834a0228fef757df9267659bb3a6bf80e7dee36
rwxmad <rwxmad@gmail.com>
2025-09-11 14:54:20 +0300

    feat(sketchybar): change space icon

--- .config/sketchybar/items/spaces.sh
+++ .config/sketchybar/items/spaces.sh
@@ -5,7 +5,7 @@
 
 # TODO: dynamic icons?
 
-SPACE_ICONS=("1 􀩼" "2 􀎬" "3 􀑬" "4 􁜾" "5 􀒋" "6 􁒃" "7 􀫓" "8 􀢅" "9 􀛸" "10")
+SPACE_ICONS=("1 􀩼" "2 􀎬" "3 􀑬" "4 􁜾" "5 􀒋" "6 􁒘" "7 􀫓" "8 􀢅" "9 􀛸" "10")
 for i in "${!SPACE_ICONS[@]}"
 do
   sid="$(($i+1))"
