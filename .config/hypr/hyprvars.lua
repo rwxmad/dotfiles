e7223bd7fc8391f5e65f1b3f7e611d2e5d97803f
rwxmad <rwxmad@gmail.com>
2026-06-17 01:11:07 +0300

    feat(hypr): add `primary` external monitor, change animations

--- .config/hypr/hyprvars.lua
+++ .config/hypr/hyprvars.lua
@@ -1,6 +1,7 @@
 -- MONITORS
 hl.monitor({ output = 'eDP-1', mode = '3200x2000@120Hz', position = '0x0', scale = 1.25 })
-hl.monitor({ output = 'DP-1', mode = '2560x1440@144Hz', position = 'auto', scale = 1 })
+-- hl.monitor({ output = 'DP-1', mode = '2560x1440@144Hz', position = 'auto', scale = 1 })
+hl.monitor({ output = 'DP-1', mode = '3840x2160@165Hz', position = 'auto', scale = 1 })
 hl.monitor({ output = '', mode = 'preferred', position = 'auto', scale = 'auto' })
 
 local f = io.open(os.getenv('HOME') .. '/.local/state/hypr/lid-internal-off')
@@ -83,18 +84,27 @@ hl.config({
 -- ANIMATIONS
 hl.config({ animations = { enabled = true } })
 
-hl.curve('overshot', { type = 'bezier', points = { { 0.05, 0.9 }, { 0.1, 1.05 } } })
-hl.curve('smoothOut', { type = 'bezier', points = { { 0.36, 0 }, { 0.66, -0.56 } } })
-hl.curve('smoothIn', { type = 'bezier', points = { { 0.25, 1 }, { 0.5, 1 } } })
+hl.curve('easeOutQuint', { type = 'bezier', points = { { 0.23, 1 }, { 0.32, 1 } } })
+hl.curve('easeInOutCubic', { type = 'bezier', points = { { 0.65, 0.05 }, { 0.36, 1 } } })
+hl.curve('linear', { type = 'bezier', points = { { 0, 0 }, { 1, 1 } } })
+hl.curve('almostLinear', { type = 'bezier', points = { { 0.5, 0.5 }, { 0.75, 1.0 } } })
+hl.curve('quick', { type = 'bezier', points = { { 0.15, 0 }, { 0.1, 1 } } })
 
-hl.animation({ leaf = 'windows', enabled = true, speed = 1, bezier = 'overshot', style = 'slide' })
-hl.animation({ leaf = 'windowsOut', enabled = true, speed = 1, bezier = 'smoothOut', style = 'slide' })
-hl.animation({ leaf = 'windowsMove', enabled = true, speed = 1, bezier = 'default' })
-hl.animation({ leaf = 'border', enabled = true, speed = 10, bezier = 'default' })
-hl.animation({ leaf = 'fade', enabled = true, speed = 2, bezier = 'smoothIn' })
-hl.animation({ leaf = 'fadeDim', enabled = true, speed = 2, bezier = 'smoothIn' })
-hl.animation({ leaf = 'workspaces', enabled = true, speed = 6, bezier = 'default' })
-hl.animation({ leaf = 'specialWorkspace', enabled = true, speed = 4, bezier = 'default', style = 'slidevert' })
+hl.animation({ leaf = 'global', enabled = true, speed = 10, bezier = 'default' })
+hl.animation({ leaf = 'border', enabled = true, speed = 5.39, bezier = 'easeOutQuint' })
+hl.animation({ leaf = 'windows', enabled = true, speed = 3.79, bezier = 'easeOutQuint' })
+hl.animation({ leaf = 'windowsIn', enabled = true, speed = 4.1, bezier = 'easeOutQuint', style = 'popin 87%' })
+hl.animation({ leaf = 'windowsOut', enabled = true, speed = 1.49, bezier = 'linear', style = 'popin 87%' })
+hl.animation({ leaf = 'fadeIn', enabled = true, speed = 1.73, bezier = 'almostLinear' })
+hl.animation({ leaf = 'fadeOut', enabled = true, speed = 1.46, bezier = 'almostLinear' })
+hl.animation({ leaf = 'fade', enabled = true, speed = 3.03, bezier = 'quick' })
+hl.animation({ leaf = 'layers', enabled = true, speed = 3.81, bezier = 'easeOutQuint' })
+hl.animation({ leaf = 'layersIn', enabled = true, speed = 4, bezier = 'easeOutQuint', style = 'fade' })
+hl.animation({ leaf = 'layersOut', enabled = true, speed = 1.5, bezier = 'linear', style = 'fade' })
+hl.animation({ leaf = 'fadeLayersIn', enabled = true, speed = 1.79, bezier = 'almostLinear' })
+hl.animation({ leaf = 'fadeLayersOut', enabled = true, speed = 1.39, bezier = 'almostLinear' })
+hl.animation({ leaf = 'workspaces', enabled = false })
+hl.animation({ leaf = 'specialWorkspace', enabled = true, speed = 3, bezier = 'easeOutQuint', style = 'slidevert' })
 
 -- DEVICE
 hl.device({
